30a04c7292c2f28c0d4982c7367ce5dab1d22c07
“Raizo-n” <sanpincha0826@gmail.com>
2023-02-23 23:36:50 -0800

    second commit

--- routes/post.js
+++ routes/post.js
@@ -3,12 +3,12 @@ const { publicPosts, privatePosts } = require("../db/post");
 const JWT = require("jsonwebtoken");
 const checkJWT = require("../middleware/checkJWT");
 
-// API for the airticle which anyone can access
+// API for the article which anyone can access
 router.get("/public", (req, res) => {
   res.send(publicPosts);
 });
 
-// API for the airticle which the person who only has JWT
+// API for the article which the person who only has JWT
 router.get("/private", checkJWT, (req, res) => {
   res.send(privatePosts);
 });
